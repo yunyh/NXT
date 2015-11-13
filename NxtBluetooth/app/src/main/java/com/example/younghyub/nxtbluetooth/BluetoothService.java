b91dc010703c5648efc6cd3fb04b3c07d1f0bd90
yunyh <dbsdudguq@gmail.com>
2015-11-14 01:45:57 +0900

    no message

---
+++ NxtBluetooth/app/src/main/java/com/example/younghyub/nxtbluetooth/BluetoothService.java
@@ -0,0 +1,112 @@
+package com.example.younghyub.nxtbluetooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Set;
+
+/**
+ * Created by YoungHyub on 2015-11-13.
+ */
+public class BluetoothService extends Handler {
+    private BluetoothAdapter mBluetoothAdapter;
+    private Activity mActivity;
+    private Handler mHandler;
+
+    //private ArrayList mDeviceNameList;
+    private ArrayAdapter<String> mArrayAdapter;
+    private ListView mListView;
+
+    public BluetoothService() {
+
+    }
+
+    public BluetoothService(Activity activity, Handler handler) {
+        mActivity = activity;
+        mHandler = handler;
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter == null){
+
+        }
+    }
+
+    public void BluetoothDeviceList() {
+        mListView = (ListView) mActivity.findViewById(R.id.device_list);
+        mArrayAdapter = new ArrayAdapter<String>(mActivity, android.R.layout.simple_list_item_1);
+        mListView.setAdapter(mArrayAdapter);
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+// If there are paired devices
+        if (pairedDevices.size() > 0) {
+            // Loop through paired devices
+            for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+                mArrayAdapter.add("Device Name : " + device.getName() + "\n"
+                        + "Address : " + device.getAddress());
+
+                //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+            }
+        }
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInput;
+        private final OutputStream mmOutput;
+
+        public ConnectThread(BluetoothSocket socket){
+            mmSocket = socket;
+
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            try{
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            }catch (IOException e){
+
+            }
+            mmInput = tmpIn;
+            mmOutput = tmpOut;
+        }
+
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+            while(true){
+                try{
+                    bytes = mmInput.read(buffer);
+                    mHandler.obtainMessage(2, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] buffer){
+            try{
+                mmOutput.write(buffer);
+            }catch (IOException e){
+
+            }
+        }
+
+        public void cancel(){
+            try{
+                mmSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ NxtBluetooth/app/src/main/java/com/example/younghyub/nxtbluetooth/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.younghyub.nxtbluetooth;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+    private BluetoothService mBluetoothService;
+    private Handler mHandler;
+
+    Button circle;
+    Button square;
+    private Button mConnect;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mHandler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+
+        mBluetoothService = new BluetoothService(this, mHandler);
+        setContentView(R.layout.activity_main);
+        circle = (Button) findViewById(R.id.btnCircle);
+        square = (Button) findViewById(R.id.btnSquare);
+        mConnect = (Button) findViewById(R.id.btnConnect);
+        mConnect.setOnClickListener(this);
+        circle.setOnClickListener(this);
+        square.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnCircle:
+                break;
+            case R.id.btnSquare:
+                break;
+            case R.id.btnConnect:
+                mBluetoothService.BluetoothDeviceList();
+                break;
+        }
+    }
+}
